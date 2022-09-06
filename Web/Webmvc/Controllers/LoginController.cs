6a8ec6a0a3c44b723de424afd8cfed0b8eb59cbc
Daniel Perez <danyyelpm@hotmail.com>
2022-09-06 17:13:27 -0500

    Se crea Login, Inicio y Usuarios(table)

---
+++ Web/Webmvc/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webmvc.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/Webmvc/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webmvc.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+    }
+}
